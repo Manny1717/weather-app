4300b29367451778cf22a7c3d9c59755e97bc713
Manuel Reyes <Reyesmanuel959@yahoo.com>
2023-12-28 00:03:24 -0800

    rounded up the degrees, gave the input styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,66 @@
-import { useSearchLocation } from "./hooks/use-search-location";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
-  const {
-    data,
-    errorMessage,
-    isLoading,
-    onLocationSearch,
-    onInputChange,
-    searchValue,
-  } = useSearchLocation();
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
 
-  // to reset data, setData(undefined)
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=de1c87324d1bebc8486c9f702c6d1b12`;
 
-  console.log("weather data -->", data);
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setLocation("");
+    }
+  };
 
   return (
     <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="Enter Location"
+          type="text"
+        />
+      </div>
       <div className="container">
         <div className="top">
           <div className="location">
-            <p>Dallas</p>
+            <p>{data.name}</p>
           </div>
           <div className="temp">
-            <h1>60°F</h1>
+            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
           </div>
           <div className="description">
-            <p>Clouds</p>
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
-        <div className="bottom">
-          <div className="feels">
-            <p className="bold">65°F</p>
-            <p>Feels Like</p>
-          </div>
-          <div className="humidity">
-            <p className="bold">20%</p>
-            <p>Humidity</p>
-          </div>
 
-          {/* setting users search value */}
-          <input onChange={onInputChange} value={searchValue} />
-          {/* <input onChange={(e) => console.log(e.target.value)} /> */}
-          {/* submitting users search value */}
-          <button onClick={onLocationSearch} disabled={isLoading}>
-            {isLoading ? "loading...." : "search"}
-          </button>
-          {errorMessage && <p className="bold">{errorMessage}</p>}
-
-          <div className="wind">
-            <p className="bold">12MPH</p>
-            <p>Wind Speed</p>
+        {data.name != undefined && (
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? (
+                <p className="bold">{data.main.feels_like.toFixed()}°F</p>
+              ) : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className="bold"> {data.main.humidity}</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.main ? (
+                <p className="bold">{data.wind.speed.toFixed()}</p>
+              ) : null}
+              <p>Wind Speed</p>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
